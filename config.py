17c3ea7737698ab8255ce6b0938750b49c35fd11
monopolybot <anas.sl8787@gmail.com>
2026-07-26 17:49:52 +0300

    Change BOT_TOKEN in config.py
    Updated the BOT_TOKEN for the bot configuration.

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 # بيانات البوت الأساسية
-BOT_TOKEN = "8393076766:AAFPrgXZ91y8lteW9nDcD7fBxAlgo-3TUGs"
+BOT_TOKEN = "8752382180:AAGTpNLCcbQPHdK3GWn-AZ3ilMfDutjz2PA"
 OWNER_ID = 5010882230
 
 # المجموعات المسموح لها باستخدام البوت
